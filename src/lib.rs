b33ddaad7cd00c38b41c7b34245f943bd75e3f30
Houtamelo <antoniopedrogf@hotmail.com>
2023-11-03 18:45:17 -0300

    Moved exports to crate::prelude

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,19 @@ pub mod disallow_click_focus;
 pub mod inspectors;
 pub mod tref_acquirer;
 
-pub use crate::bounded_isize::*;
-pub use crate::bounded_u32::*;
-pub use crate::clamp01::*;
-pub use crate::disallow_click_focus::*;
-pub use crate::inspectors::*;
-pub use crate::tref_acquirer::*;
-
 fn init(handle: InitHandle) {
-	handle.add_class::<DisallowClickFocus>();
+	handle.add_class::<prelude::DisallowClickFocus>();
 }
 
 godot_gdnative_init!(_ as houtamelo_utils_gdnative_init);
 godot_nativescript_init!(init as houtamelo_utils_gdnative_nativescript_init);
 godot_gdnative_terminate!(_ as houtamelo_utils_gdnative_terminate);
+
+pub mod prelude {
+	pub use crate::bounded_isize::*;
+	pub use crate::bounded_u32::*;
+	pub use crate::clamp01::*;
+	pub use crate::disallow_click_focus::*;
+	pub use crate::inspectors::*;
+	pub use crate::tref_acquirer::*;
+}
