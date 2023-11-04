d6a2d5e8014b88baddecd5624e535505b2a954f2
Houtamelo <antoniopedrogf@hotmail.com>
2023-11-04 16:44:10 -0300

    impl tref_acquirers for Option<&Ref<_>>

--- src/tref_acquirer.rs
+++ src/tref_acquirer.rs
@@ -18,11 +18,35 @@ impl<T> AssertSaneTRef<T> for Option<Ref<T, Shared>> where T : GodotObject<Memor
 	}
 }
 
+impl<T> AssertSaneTRef<T> for Option<&Ref<T, Shared>> where T : GodotObject<Memory = ManuallyManaged> {
+	fn assert_tref_if_sane(&self) -> Option<TRef<T, Shared>> {
+		if let Some(obj_ref) = self {
+			if let Some(obj_tref) = unsafe { obj_ref.assume_safe_if_sane() } {
+				return Some(obj_tref);
+			}
+		}
+
+		godot_error!("Failed to get object through sane assertion, input: {self:?}");
+		return None;
+	}
+}
+
 pub trait AssertSafeTRef<T> where T : GodotObject<Memory = RefCounted> {
 	fn assert_tref(&self) -> Option<TRef<T, Shared>>;
 }
 
 impl<T> AssertSafeTRef<T> for Option<Ref<T, Shared>> where T : GodotObject<Memory = RefCounted> {
+	fn assert_tref(&self) -> Option<TRef<T, Shared>> {
+		if let Some(obj_ref) = self {
+			return Some(unsafe { obj_ref.assume_safe() });
+		}
+
+		godot_error!("Failed to get object, input: {self:?}");
+		return None;
+	}
+}
+
+impl<T> AssertSafeTRef<T> for Option<&Ref<T, Shared>> where T : GodotObject<Memory = RefCounted> {
 	fn assert_tref(&self) -> Option<TRef<T, Shared>> {
 		if let Some(obj_ref) = self {
 			return Some(unsafe { obj_ref.assume_safe() });
@@ -49,11 +73,33 @@ impl<T> TrySaneTRef<T> for Option<Ref<T, Shared>> where T : GodotObject<Memory =
 	}
 }
 
+impl<T> TrySaneTRef<T> for Option<&Ref<T, Shared>> where T : GodotObject<Memory = ManuallyManaged> {
+	fn try_tref_if_sane(&self) -> Option<TRef<T, Shared>> {
+		if let Some(obj_ref) = self {
+			if let Some(obj_tref) = unsafe { obj_ref.assume_safe_if_sane() } {
+				return Some(obj_tref);
+			}
+		}
+
+		return None;
+	}
+}
+
 pub trait TrySafeTRef<T> where T : GodotObject<Memory = RefCounted> {
 	fn try_tref(&self) -> Option<TRef<T, Shared>>;
 }
 
 impl<T> TrySafeTRef<T> for Option<Ref<T, Shared>> where T: GodotObject<Memory = RefCounted> {
+	fn try_tref(&self) -> Option<TRef<T, Shared>> {
+		if let Some(obj_ref) = self {
+			return Some(unsafe { obj_ref.assume_safe() });
+		}
+
+		return None;
+	}
+}
+
+impl<T> TrySafeTRef<T> for Option<&Ref<T, Shared>> where T: GodotObject<Memory = RefCounted> {
 	fn try_tref(&self) -> Option<TRef<T, Shared>> {
 		if let Some(obj_ref) = self {
 			return Some(unsafe { obj_ref.assume_safe() });
@@ -69,6 +115,18 @@ pub trait AssertTInstance<T> where T : NativeClass {
 }
 
 impl<T> AssertTInstance<T> for Option<Instance<T>> where T : NativeClass {
+	fn assert_tinstance<'a, 'r>(&'r self) -> Option<TInstance<'a, T, Shared>>
+	                            where AssumeSafeLifetime<'a, 'r>: LifetimeConstraint<<T::Base as GodotObject>::Memory> {
+		if let Some(obj_ref) = self {
+			return Some(unsafe { obj_ref.assume_safe() });
+		}
+
+		godot_error!("Failed to get instance.");
+		return None;
+	}
+}
+
+impl<T> AssertTInstance<T> for Option<&Instance<T>> where T : NativeClass {
 	fn assert_tinstance<'a, 'r>(&'r self) -> Option<TInstance<'a, T, Shared>>
 	                            where AssumeSafeLifetime<'a, 'r>: LifetimeConstraint<<T::Base as GodotObject>::Memory> {
 		if let Some(obj_ref) = self {
