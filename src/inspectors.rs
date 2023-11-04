b55f553d4fe3e4bb0f60b0dc0d3a34bc7edaa27b
Houtamelo <antoniopedrogf@hotmail.com>
2023-11-04 17:04:57 -0300

    impl inspectors for Option<&Ref<_>> and Result<&T, Err>

--- src/inspectors.rs
+++ src/inspectors.rs
@@ -5,7 +5,6 @@ use super::tref_acquirer::*;
 
 pub trait SomeInspector<T> {
 	fn on_some(&self, closure: impl FnOnce(&T));
-	fn on_some_mut(&mut self, closure: impl FnOnce(&mut T));
 }
 
 impl<T> SomeInspector<T> for Option<T> {
@@ -14,7 +13,21 @@ impl<T> SomeInspector<T> for Option<T> {
 			closure(value);
 		}
 	}
+}
 
+impl<T> SomeInspector<T> for Option<&T> {
+	fn on_some(&self, closure: impl FnOnce(&T)) {
+		if let Some(value) = self {
+			closure(value);
+		}
+	}
+}
+
+pub trait SomeMutInspector<T> {
+	fn on_some_mut(&mut self, closure: impl FnOnce(&mut T));
+}
+
+impl<T> SomeMutInspector<T> for Option<T> {
 	fn on_some_mut(&mut self, closure: impl FnOnce(&mut T)) {
 		if let Some(value) = self {
 			closure(value);
@@ -27,8 +40,23 @@ pub trait GodotManualSomeInspector<T> where T: GodotObject<Memory = ManuallyMana
 	fn map_on_sane<U>(&self, closure: impl FnOnce(TRef<T>) -> U) -> Option<U>;
 }
 
-
 impl <T> GodotManualSomeInspector<T> for Option<Ref<T>> where T: GodotObject<Memory = ManuallyManaged>  {
+	fn on_sane(&self, closure: impl FnOnce(TRef<T>)) {
+		if let Some(value) = self.assert_tref_if_sane() {
+			closure(value);
+		}
+	}
+
+	fn map_on_sane<U>(&self, closure: impl FnOnce(TRef<T>) -> U) -> Option<U> {
+		if let Some(value) = self.assert_tref_if_sane() {
+			return Some(closure(value));
+		}
+
+		return None;
+	}
+}
+
+impl <T> GodotManualSomeInspector<T> for Option<&Ref<T>> where T: GodotObject<Memory = ManuallyManaged>  {
 	fn on_sane(&self, closure: impl FnOnce(TRef<T>)) {
 		if let Some(value) = self.assert_tref_if_sane() {
 			closure(value);
@@ -50,6 +78,22 @@ pub trait GodotRefCountedSomeInspector<T> where T: GodotObject<Memory = RefCount
 }
 
 impl <T> GodotRefCountedSomeInspector<T> for Option<Ref<T>> where T: GodotObject<Memory = RefCounted>  {
+	fn on_safe(&self, closure: impl FnOnce(TRef<T>)) {
+		if let Some(value) = self.assert_tref() {
+			closure(value);
+		}
+	}
+
+	fn map_on_safe<U>(&self, closure: impl FnOnce(TRef<T>) -> U) -> Option<U> {
+		if let Some(value) = self.assert_tref() {
+			return Some(closure(value));
+		}
+
+		return None;
+	}
+}
+
+impl <T> GodotRefCountedSomeInspector<T> for Option<&Ref<T>> where T: GodotObject<Memory = RefCounted>  {
 	fn on_safe(&self, closure: impl FnOnce(TRef<T>)) {
 		if let Some(value) = self.assert_tref() {
 			closure(value);
@@ -91,9 +135,26 @@ impl <'a, 'r, T> GodotInstanceSomeInspector<'a, 'r, T> for Option<Instance<T>>
 	}
 }
 
+impl <'a, 'r, T> GodotInstanceSomeInspector<'a, 'r, T> for Option<&Instance<T>>
+	where T:: UserData:Map,
+	      T:: UserData:MapMut,
+	      T: NativeClass,
+	      AssumeSafeLifetime<'a, 'r>: LifetimeConstraint<<T::Base as GodotObject>::Memory> {
+	fn on_safe<U>(&'r self, closure: impl FnOnce(&T, TRef<'_, <T as gdnative::prelude::NativeClass>::Base>) -> U) {
+		if let Some(value) = self.assert_tinstance() {
+			let _ = value.map(closure);
+		}
+	}
+
+	fn on_safe_mut<U>(&'r self, closure: impl FnOnce(&mut T, TRef<'_, <T as gdnative::prelude::NativeClass>::Base>) -> U) {
+		if let Some(value) = self.assert_tinstance() {
+			let _ = value.map_mut(closure);
+		}
+	}
+}
+
 pub trait OkInspector<T> {
 	fn on_ok(&self, closure: impl FnOnce(&T));
-	fn on_ok_mut(&mut self, closure: impl FnOnce(&mut T));
 }
 
 impl <TValue, TError> OkInspector<TValue> for Result<TValue, TError> {
@@ -102,7 +163,21 @@ impl <TValue, TError> OkInspector<TValue> for Result<TValue, TError> {
 			closure(value);
 		}
 	}
+}
 
+impl <TValue, TError> OkInspector<TValue> for Result<&TValue, TError> {
+	fn on_ok(&self, closure: impl FnOnce(&TValue)) {
+		if let Ok(value) = self {
+			closure(value);
+		}
+	}
+}
+
+pub trait OkMutInspector<T> {
+	fn on_ok_mut(&mut self, closure: impl FnOnce(&mut T));
+}
+
+impl <TValue, TError> OkMutInspector<TValue> for Result<TValue, TError> {
 	fn on_ok_mut(&mut self, closure: impl FnOnce(&mut TValue)) {
 		if let Ok(value) = self {
 			closure(value);
