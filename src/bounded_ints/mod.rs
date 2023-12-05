68a918ecd61937f3f5d1f2cf372cf78560e7001a
Houtamelo <antoniopedrogf@hotmail.com>
2023-12-05 18:07:34 -0300

    moved bounded ints and clamp01 to generic crate "houtamelo_utils"

--- src/bounded_ints/mod.rs
+++
@@ -1,3 +0,0 @@
-pub(crate) mod macros;
-
-pub use macros::{BoundUSize, BoundU32, BoundU64, BoundISize, BoundI32, BoundI64};
--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,12 @@
 use gdnative::init::{godot_gdnative_init, godot_gdnative_terminate, godot_nativescript_init };
 use gdnative::prelude::*;
 
-pub(crate) mod bounded_ints;
-
-pub mod clamp01;
-pub mod hashset_macro;
 pub mod disallow_click_focus;
 pub mod inspectors;
 pub mod tref_acquirer;
 pub mod tref_unwrapper;
 pub mod godot_log_macros;
 pub mod extensions;
-pub mod matches_macro;
 
 fn init(handle: InitHandle) {
 	handle.add_class::<prelude::DisallowClickFocus>();
@@ -24,19 +19,15 @@ godot_nativescript_init!(init as houtamelo_utils_gdnative_nativescript_init);
 godot_gdnative_terminate!(_ as houtamelo_utils_gdnative_terminate);
 
 pub mod prelude {
-	pub use crate::bounded_ints::*;
 	pub use crate::inspectors::*;
 	pub use crate::inspectors::option_impls::*;
 	pub use crate::inspectors::manual::*;
 	pub use crate::inspectors::refcounted::*;
 	pub use crate::inspectors::instance::*;
-	pub use crate::clamp01::*;
 	pub use crate::disallow_click_focus::*;
 	pub use crate::godot_error_get;
 	pub use crate::godot_panic;
-	pub use crate::hash_set;
 	pub use crate::tref_acquirer::*;
 	pub use crate::tref_unwrapper::*;
 	pub use crate::extensions::*;
-	pub use crate::any_matches;
 }
